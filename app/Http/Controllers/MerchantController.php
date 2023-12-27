39695cfde8d9e820a191cb015abe2d956fb5359e
Glorify <ehtishamhussain59@gmail.com>
2023-12-27 14:42:11 +0500

    all tests passed

--- app/Http/Controllers/MerchantController.php
+++ app/Http/Controllers/MerchantController.php
@@ -6,6 +6,7 @@ use App\Models\Merchant;
 use App\Services\MerchantService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Models\Order;
 use Illuminate\Support\Carbon;
 
 class MerchantController extends Controller
@@ -16,12 +17,38 @@ class MerchantController extends Controller
 
     /**
      * Useful order statistics for the merchant API.
-     * 
+     *
      * @param Request $request Will include a from and to date
      * @return JsonResponse Should be in the form {count: total number of orders in range, commission_owed: amount of unpaid commissions for orders with an affiliate, revenue: sum order subtotals}
      */
     public function orderStats(Request $request): JsonResponse
     {
         // TODO: Complete this method
+
+        $from = $request->input('from');
+        $to = $request->input('to');
+
+        // Get the authenticated user
+        $user = auth()->user();
+
+        $merchant = $user->merchant;
+
+        $orders = Order::where('merchant_id', $merchant->id)
+            ->whereBetween('created_at', [$from, $to])
+            ->get();
+
+        $count = $orders->count();
+        $revenue = $orders->sum('subtotal');
+        $commissionsOwed = $orders->filter(function ($order) {
+            return $order->affiliate_id !== null;
+        })->sum('commission_owed');
+
+        $response = [
+            'count' => $count,
+            'revenue' => $revenue,
+            'commissions_owed' => $commissionsOwed,
+        ];
+
+        return response()->json($response);
     }
 }
